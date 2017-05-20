9b206e4b4daaf87f5faae054acf67fb329dbeb6f
cynthiatliu <cynliu98@mit.edu>
2017-05-20 15:29:09 -0400

    Algorithm v. 1 It mostly works. More testing needs to be done.

--- general_mat_mult_2.py
+++ general_mat_mult_2.py
@@ -1,5 +1,5 @@
-#UROP Spring 2017
-#5/16/17
+#UROP Spring 717
+#5/19/17
 #Generalized matrix multiplication
 
 import itertools
@@ -21,7 +21,9 @@ class solutionTuple(object):
         for j in range(len(self.sol)):
             sol = self.sol[j]
             for i in range(len(sol)):
-                tortn += str(sol[i])
+                if sol[i] is None:
+                    tortn += str(sol[i])
+                else: tortn += str('%.3f'%(sol[i]))
                 if i != len(sol)-1:
                     tortn += ", "
             tortn += ")"
@@ -126,128 +128,6 @@ def steadyStateTest(orderedvars, dim):
 
 # Generalized matrix multiplication
 # A times B (labelled tensors)
-def matMultBad(A,B,exposed):
-    avars = A.dims; bvars = B.dims;
-    aresos = A.resolutions; bresos = B.resolutions
-    allvar = removeDupsOrder(avars + bvars) # in order, removed duplicates
-
-    allresos = []
-    for v in allvar:
-        if v in avars:
-            allresos.append(aresos[avars.index(v)])
-        elif v in bvars:
-            allresos.append(bresos[bvars.index(v)])
-    
-    for e in exposed: # all exposed variables must be variables
-        assert(e in allvar)
-    indexers = [i for i in allvar if not (i in exposed)] # in order
-##    print ("avars " + str(avars))
-##    print ("bvars " + str(bvars))
-##    print ("exposed vars " + str(exposed))
-    print ("indexers " + str(indexers))
-##    print ("All variables " + str(allvar))
-##    print ("All resolutions " + str(allresos)) # also in order      
-
-    matA = A.tensor; matB = B.tensor
-    exposedi = [allvar.index(e) for e in exposed]
-    indexi = [allvar.index(i) for i in indexers]
-    exresos = [allresos[e] for e in exposedi]
-    inresos = [allresos[i] for i in indexi]
-##    print ("exposed indices: " + str(exposedi))
-##    print ("indexer indicies: " + str(indexi))
-    
-    # Initialize empty numpy array (numpy.empty) with shape
-    # Number of dimensions = Number of exposed variables + 1
-    # Length of each dimension = resolution of that variable
-    # Note that the final dimension is for containing the multiple variables
-    # In a solution for a given set of boundary conditions
-    rawshape = []
-    for i in exposedi:
-        rawshape.append(allresos[i])
-
-    rawshape.append(1)
-    shape = tuple(rawshape)
-    # print ("Shape of solution array: " + str(shape))
-    Usol = np.empty(shape)
-    Usol = Usol.tolist()
-    # print (str(Usol))
-
-    # THIS PART IS WRONG    
-    # for each set of boundary conditions in Usol:
-##    for bci in np.ndenumerate(Usol):
-##        bci = list(list(bci)[0])[0:len(exposed)] #extract tuple of bc indices
-##        # bc = the bci[0] through bci[n] values of exposed
-##
-##        bcallv = []
-##        for v in exposed:
-##            if (v in avars):
-##                bcallv.append(A.extractDim(0,5,v))
-##            elif (v in bvars):
-##                bcallv.append(B.extractDim(0,5,v))
-##        assert (len(bci) == len(bcallv))
-##        
-##        # from the possible values of each dimension bcallv[i],
-##        # extract the bci[i]th value (the index of the boundary condition value)
-##        bc = [bcallv[i][bci[i]] for i in range(len(bci))]
-##        # print ("Boundary conditions: " + str(bc))
-##
-##        indvals = []
-##        for ind in indexers:
-##            indvals.append(A.extractDim(0,5,ind))
-##
-##        good = False
-##    #   for tup in indexers:
-##        for itup in itertools.product(*indvals):
-##            # print ("Tuple of indexer values: " + str(itup))
-##            itup = list(itup)
-##
-##            # want - the program goes through each variable NAME in A and B
-##            # and replaces it with the correct VALUE from either
-##            # bc or itup
-##            valsA = []; valsB = []
-##            for va in avars:
-##                if (va in exposed):
-##                    valsA.append(bc[exposed.index(va)])
-##                elif (va in indexers):
-##                    valsA.append(itup[indexers.index(va)])
-##
-##            for vb in bvars:
-##                if (vb in exposed):
-##                    valsB.append(bc[exposed.index(vb)])
-##                elif (vb in indexers):
-##                    valsB.append(itup[indexers.index(vb)])
-##
-##            print ("Values for A: " + str(valsA))
-##            print ("Values for B: " + str(valsB))
-##
-##            if (steadyStateTest(valsA) and steadyStateTest(valsB)):
-##                good = True
-##                allvalues = [] # the solution values
-##                for v in allvar:
-##                    if (v in avars):
-##                        allvalues.append(valsA[avars.index(v)])
-##                    elif (v in bvars):
-##                        allvalues.append(valsB[bvars.index(v)])
-##
-##                UpSol = Usol #UPrime Solution
-##                for bcv in bci:
-##                    UpSol = UpSol[bcv]
-##
-##                if (type(UpSol[0]) == float):
-##                    UpSol[0] = tuple(allvalues)
-##                else: UpSol.append(tuple(allvalues)) # append the solution
-##                # print ("Upsol: " + str(UpSol))
-##
-##        if (not(good)):
-##            UpSol = Usol
-##            for bcv in bci:
-##                UpSol = UpSol[bcv]
-##
-##            UpSol[0] = []
-                
-    Usol = np.array(Usol)
-    return labeledTensor(Usol, exposed, exresos)
-
 def matMult(A, B, exposed):
     # Preparation
     matA = A.tensor; matB = B.tensor # what we're actually multiplying
@@ -352,10 +232,26 @@ def main():
 
     U1 = labeledTensor(rU1, ['i','j','k'], [7,7,7])
     U2 = labeledTensor(rU2, ['j','k','l'], [7,7,7])
+    U3 = labeledTensor(rU1, ['k','l','m'], [7,7,7])
+    U4 = labeledTensor(rU1, ['l','m','n'], [7,7,7])
 
-    foo = matMult(U1,U2,['i','k','l'])
-    for e in foo.getTensor().flatten():
-        print (str(e))
-    # print (str(foo.getTensor()))
+##    U12 = matMult(U1,U2,['i','k','l'])
+##    U34 = matMult(U3,U4,['k','l','n'])
+##    USol = matMult(U12,U34,['i','n'])
+
+    U12 = matMult(U1,U2,['i','k','l'])
+    U123 = matMult(U12,U3,['i','l','m'])
+    USol = matMult(U123,U4,['i','n'])
+    
+    i = 0
+    for e in USol.getTensor().flatten():
+        leftc = i//7; rightc = i%7
+        left = '%.3f'%(dim1[leftc])
+        right = '%.3f'%(dim1[rightc])
+        print ("Value for bc's (" + str(left) + ", " +
+               str(right) + "): " + str(e))
+        i += 1
+    print ("Finished")
+    # print (str(USol.getTensor()))
 
 main()
